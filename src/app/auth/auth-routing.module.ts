0a2e88e5be390d60449f51ff99cd3dc51c0cff63
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-05-29 21:32:00 -0500

    created ui for signup form

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component'
+
+const routes: Routes = [
+  { path: 'signup', component: SignupComponent},
+  { path: '', component: SigninComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: 
+  [
+    RouterModule.forChild(routes)
+  ],
+  exports: 
+  [
+    RouterModule
+  ]
 })
 export class AuthRoutingModule { }
