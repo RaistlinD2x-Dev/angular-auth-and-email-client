093c965cda2e2b797804503401ea5a62f97d67b6
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-05-30 00:32:45 -0500

    finished asynchronous validation of username

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AsyncValidator, FormControl } from '@angular/forms';
+import {  map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class UniqueUsername implements AsyncValidator {
@@ -9,9 +11,19 @@ export class UniqueUsername implements AsyncValidator {
     validate = (control: FormControl) => {
         const url = 'https://api.angular-email.com/auth/username';
         const { value } = control;
-        
+
         return this.http.post<any>(url, { 
             username: value
-        })
+        }).pipe(
+            map(() => {
+                return null;
+            }),
+            catchError((err) => {
+                if(err.error.username) {
+                    return of({ nonUniqueUsername: true });
+                }
+                return of({ noConnection: true });
+            })
+        )
     }
 }
