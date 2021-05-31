33588b8f444f0f1915cd6f373bba708ae45205cf
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-05-30 23:35:28 -0500

    setup Observer for HttpInterceptor just for reference

--- src/app/auth/auth-http-inceptor.ts
+++ src/app/auth/auth-http-inceptor.ts
@@ -4,8 +4,10 @@ import {
     HttpInterceptor,
     HttpHandler,
     HttpRequest,
+    HttpEventType,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthHttpInceptor implements HttpInterceptor {
@@ -19,6 +21,18 @@ export class AuthHttpInceptor implements HttpInterceptor {
         })
 
         console.log(modifiedReq)
-        return next.handle(modifiedReq);
+        return next.handle(modifiedReq)
+            .pipe(
+                // filter(val => val.type === HttpEventType.Sent),
+                tap(val => {
+                    if(val.type === HttpEventType.Sent) {
+                        console.log('Request was sent to server')
+                    }
+
+                    if (val.type === HttpEventType.Response){
+                        console.log('Got a response from the API: ', val)
+                    }
+                })
+            )
     }
 }
