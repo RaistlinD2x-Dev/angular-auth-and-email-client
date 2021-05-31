0bee4ce96eabb9a0b3e2beee4be5e135ab263a33
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-05-31 12:27:26 -0500

    added getEmails() to email service

--- src/app/inbox/email.service.ts
+++ src/app/inbox/email.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
+interface EmailSummary { 
+  id: string;
+  subject: string;
+  from: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,4 +14,8 @@ export class EmailService {
   emailUrl: string = 'https://api.angular-email.com/emails'
 
   constructor(private http: HttpClient) { }
+
+  getEmails() {
+    return this.http.get<EmailSummary[]>(this.emailUrl)
+  }
 }
